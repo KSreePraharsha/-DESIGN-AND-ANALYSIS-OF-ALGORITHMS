48e2061975b341999e002b8daa500f9da3edde20
KSreePraharsha <krothapallisreepraharsha@gmail.com>
2026-08-22 15:02:20 +0530

    Implement queue operations using linked list

---
+++ Week-3/Queue operations using Linked List.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+struct Node *front = NULL;
+struct Node *rear = NULL;
+
+void enqueue(int value) {
+    struct Node *newNode;
+    newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+
+    if (rear == NULL) {
+        front = rear = newNode;
+    } else {
+        rear->next = newNode;
+        rear = newNode;
+    }
+}
+void dequeue() {
+    struct Node *temp;
+    if (front == NULL) {
+        printf("Queue Underflow\n");
+        return;
+    }
+    temp = front;
+    printf("%d element deleted\n", front->data);
+    front = front->next;
+
+    if (front == NULL) {
+        rear = NULL;
+    }
+    free(temp);
+}
+void display() {
+    struct Node *temp = front;
+    if (front == NULL) {
+        printf("Queue is empty\n");
+        return;
+    }
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+int main() {
+    int choice, value;
+    while (1) {
+        printf("\n1. Enqueue  2. Dequeue  3. Display  4. Exit\n");
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1: printf("Enter value: ");
+                    scanf("%d", &value);
+                    enqueue(value);
+                    printf("%d element inserted\n", value);
+                    break;
+
+            case 2: dequeue(); break;
+
+            case 3: display(); break;
+
+            case 4: exit(0);
+
+            default: printf("Invalid choice\n");
+        }
+    }
+    return 0;
+}
